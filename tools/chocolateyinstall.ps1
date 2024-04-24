d5c0bc442f3ba3d9c3a8630e98977578bf1fe25e
Ivan Sučić <f1sucic@gmail.com>
2024-04-24 20:14:59 +0200

    Built lldb-mi for windows x64

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,39 @@
+﻿$ErrorActionPreference = 'Stop' # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$zipArchive = Join-Path $toolsDir -ChildPath 'lldb-mi.zip'
+$installPath = "$env:ProgramFiles\lldb-mi\bin"
+$miDebuggerPath = Join-Path $installPath.replace("\","\\") '\\lldb-mi.exe' 
+
+if ((Get-WmiObject win32_operatingsystem | select osarchitecture).osarchitecture -eq "64-bit")
+{
+    $unzipArgs = @{
+		FileFullPath = $zipArchive
+		Destination = $installPath
+	}
+
+	If(!(test-path -PathType container $installPath))
+	{
+		New-Item -ItemType Directory -Path $installPath
+	}
+	
+	Get-ChocolateyUnzip @unzipArgs
+	
+	Write-Host @"
+	
+	Thank you for installing!
+
+	To use with vs-code do following:
+		1. Install Clang/LLVM toolschain
+		2. Set the environment variable LLVMInstallDir
+		   to toolchain path.
+		3. In launch.json add:
+		   "MIMode": "lldb",
+		   "miDebuggerPath": "$miDebuggerPath"
+
+	Happy debugging!
+"@
+}
+else
+{
+    throw 'Error: This package requires an 64-bit os!'
+}
---
+++ tools/chocolateyuninstall.ps1
@@ -0,0 +1,23 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$installPathStr = "$env:ProgramFiles\lldb-mi"
+$installPath = @{Path = $installPathStr}
+$installBinPathStr = "$installPathStr\bin"
+$installBinPath = @{Path = $installBinPathStr}
+$installExePathStr = "$installBinPathStr\lldb-mi.exe" 
+$installExePath = @{Path = $installExePathStr}
+
+Remove-Item @installExePath
+
+$directoryInfo = Get-ChildItem @installBinPath | Measure-Object
+
+if ($directoryInfo.count -eq 0)
+{
+	Remove-Item @installBinPath
+	
+	$directoryInfo = Get-ChildItem @installPath | Measure-Object
+	if ($directoryInfo.count -eq 0) 
+	{
+		Remove-Item @installPath
+	}
+}
